b90043664c7b296eb92338c4912abffb16cfc90e
Jack Thorne <jack@kilmer.co>
2021-01-27 13:44:43 -0600

    update help documentation

--- src/cli.cr
+++ src/cli.cr
@@ -38,33 +38,35 @@ class Braintree::CLI
     command = Command::Banner
     main_parser = OptionParser.parse do |parser|
       parser.banner = "Usage: bt [command] [switches]"
-      parser.on("-h", "--help", "Show this help") {
+      parser.on("-h", "--help", "Prints this dialog") {
         command = Command::Banner
         banner = parser.to_s
       }
       parser.on("-v", "--version", "Print version") { puts parser }
-      parser.on("-d", "--debug", "Print version") { ::Log.setup(:debug) }
-      parser.on("dispute", "Subcommand for disputes") do
+      parser.on("-d", "--debug", "show debugging information") { ::Log.setup(:debug) }
+
+      parser.separator("Subcommands")
+      parser.on("dispute", "Interact with disputes") do
         parser.banner = "Usage: bt disputes [subcommand|ids] [switches]"
-        parser.on("-h", "--help", "Show this help") {
+        parser.on("-h", "--help", "Prints this dialog") {
           command = Command::Banner
           banner = parser.to_s
         }
-        parser.separator("Global Values")
+        parser.separator("Global")
         parser.on("-l", "--local", "use local data") { opts[:source] = "local" }
         parser.on("-s", "--store", "persist the response") { opts[:source] = "remote" }
-        parser.separator("Dispute Actions")
+        parser.separator("Actions")
         parser.on("-F", "--finalize", "finalizes the dispute") { command = Command::DisputeFinalize }
         parser.on("-A", "--accept", "accepts a dispute") { command = Command::DisputeAccept }
 
         parser.on("create", "create a new dispute") do
           command = Command::DisputeCreate
           parser.banner = "Usage: bt disputes create [switches]"
-          parser.on("-h", "--help", "Show this help") {
+          parser.on("-h", "--help", "Prints this dialog") {
             command = Command::Banner
             banner = parser.to_s
           }
-          parser.separator("Dispute Attributes")
+          parser.separator("Attributes")
           parser.on("-a AMOUNT", "--amount AMOUNT", "set amount for dispute") { |_a| opts[:amount] = _a }
           parser.on("-n NUM", "--number NUM", "set card number for dispute") { |_n| opts[:number] = _n }
           parser.on("-e DATE", "--exp_date DATE", "set expiration date for dispute") { |_e| opts[:exp_date] = _e }
@@ -73,7 +75,7 @@ class Braintree::CLI
 
         parser.on("evidence", "adds file evidence") do
           command = Command::DisputeEvidence
-          parser.on("-h", "--help", "Show this help") {
+          parser.on("-h", "--help", "Prints this dialog") {
             command = Command::Banner
             banner = parser.to_s
           }
@@ -83,32 +85,33 @@ class Braintree::CLI
           parser.on("-r", "--remove", "removes evidence for dispute") { |_r| opts[:remove] = _r }
         end
 
+        parser.separator("Subcommands")
         parser.on("search", "searches disputes") do
           command = Command::DisputeSearch
-          parser.on("-h", "--help", "Show this help") {
+          parser.on("-h", "--help", "Prints this dialog") {
             command = Command::Banner
             banner = parser.to_s
           }
           parser.separator("Search Criteria")
           parser.on("-a AMOUNT", "--amount AMOUNT", "amount range (100,200)") { |_a| opts[:amount] = _a }
           parser.on("-s STATUS", "--status STATUS", "status (open,won,lost)") { |_s| opts[:status] = _s }
-          parser.on("-c CASE_NUMBER", "--case CASE_NUMBER", "case number") { |_c| opts[:case_number] = _c }
-          parser.on("-C CUSTOMER_ID", "--customer CUSTOMER_ID", "customer id") { |_c| opts[:customer_id] = _c }
-          parser.on("-d DISBURSMENT_DATE", "--disbursment_date DISBURSMENT_DATE", "disbursment date") { |_d| opts[:disbursment_date] = _d }
-          parser.on("-s EFFECTIVE_DATE", "--effective_date EFFECTIVE_DATE", "effective date") { |_e| opts[:effective_date] = _e }
+          parser.on("-c NUM", "--case NUM", "case number") { |_c| opts[:case_number] = _c }
+          parser.on("-C ID", "--customer ID", "customer id") { |_c| opts[:customer_id] = _c }
+          parser.on("-d DATE", "--disbursment_date DATE", "disbursment date") { |_d| opts[:disbursment_date] = _d }
+          parser.on("-s DATE", "--effective_date DATE", "effective date") { |_e| opts[:effective_date] = _e }
           parser.on("-i ID", "--id ID", "dispute id") { |_i| opts[:dispute_id] = _i }
           parser.on("-k KIND", "--kind KIND", "kind") do |_k|
             opts[:kind] = opts[:kind]? ? "#{opts[:kind]},#{_k}" : opts[:kind]
           end
-          parser.on("-m MERCHANT_ACCOUNT_ID", "--merchant_account_id MERCHANT_ACCOUNT_ID", "merchant account id") { |_k| opts[:merchant_account_id] = _k }
+          parser.on("-m ID", "--merchant_account_id ID", "merchant account id") { |_k| opts[:merchant_account_id] = _k }
           parser.on("-r REASON", "--reason REASON", "reason") { |_r| opts[:reason] = _r }
-          parser.on("-R REASON_CODE", "--reason_code REASON_CODE", "reason_code") { |_r| opts[:reason_code] = _r }
-          parser.on("-D RECEIVED_DATE", "--received_date RECEIVED_DATE", "received date") { |_r| opts[:received_date] = _r }
-          parser.on("-n REFERENCE_NUMBER", "--reference_number REFERENCE_NUMBER", "reference number") { |_r| opts[:reference_number] = _r }
-          parser.on("-b REPLY_BY_DATE", "--reply_by_date REPLY_BY_DATE", "reply by date") { |_r| opts[:reply_by_date] = _r }
+          parser.on("-R CODE", "--reason_code CODE", "reason_code") { |_r| opts[:reason_code] = _r }
+          parser.on("-D DATE", "--received_date DATE", "received date") { |_r| opts[:received_date] = _r }
+          parser.on("-n NUM", "--reference_number NUM", "reference number") { |_r| opts[:reference_number] = _r }
+          parser.on("-b DATE", "--reply_by_date DATE", "reply by date") { |_r| opts[:reply_by_date] = _r }
           parser.on("-S STATUS", "--status STATUS", "status") { |_s| opts[:status] = _s }
-          parser.on("-t TRANSACTION_ID", "--transaction_id TRANSACTION_ID", "transaction_id") { |_t| opts[:transaction_id] = _t }
-          parser.on("-T TRANSACTION_SOURCE", "--transaction_source TRANSACTION_SOURCE", "transaction_source") { |_t| opts[:transaction_source] = _t }
+          parser.on("-t ID", "--transaction_id ID", "transaction_id") { |_t| opts[:transaction_id] = _t }
+          parser.on("-T SOURCE", "--transaction_source SOURCE", "transaction_source") { |_t| opts[:transaction_source] = _t }
         end
 
         parser.unknown_args do |pre_dash, post_dash|
